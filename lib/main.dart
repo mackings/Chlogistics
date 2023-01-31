87e8fb0bbf875b589b37d2a9ddafbd02911594a0
mackingsley <kingsleyudoma2018@gmail.com>
2023-01-31 13:31:01 +0100

    ios

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
-  runApp(
+  runApp(  
     ProviderScope(child: MyApp())
   );
   await WidgetsFlutterBinding.ensureInitialized();
