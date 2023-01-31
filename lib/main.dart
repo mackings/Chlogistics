c700818f8539969cf6280bc1bef20ddb88035a43
mackings <kingsleyudoma2018@gmail.com>
2023-01-31 13:27:06 +0100

    ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
+import 'package:chstores/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
-  runApp(
-    ProviderScope(child: MyApp())
-  );
-  await WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  runApp(ProviderScope(child: MyApp()));
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
 
 class MyApp extends StatelessWidget {
@@ -20,11 +19,11 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
-        //
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
         // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // "hot reload" (press "r" in the consol
+        //e where you ran "flutter run",
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
